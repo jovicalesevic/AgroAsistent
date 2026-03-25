29a926fcb4776b4932602270c084957dc9f106f1
Jovica Lesevic <jovica.lesevic@gmail.com>
2026-03-25 18:39:24 +0100

    fix: CORS dozvoljava client URL

--- server/server.js
+++ server/server.js
@@ -7,7 +7,14 @@ const { clerkMiddleware } = require("./middleware/auth");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    process.env.CLIENT_URL
+  ].filter(Boolean)
+}));
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
