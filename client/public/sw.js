899f2a64a787c956a1558f76062f5a40d55cd1e4
Jovica Lesevic <jovica.lesevic@gmail.com>
2026-03-28 06:48:29 +0100

    feat: PWA setup - manifest, service worker, index.html

---
+++ client/public/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'agroasistent-v1'
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS)
+    })
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    })
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url)
+  if (url.pathname.startsWith('/api/')) return
+  if (url.origin !== self.location.origin) return
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (event.request.method === 'GET' && response.ok) {
+          const clone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone))
+        }
+        return response
+      })
+      .catch(() => caches.match(event.request))
+  )
+})
+
