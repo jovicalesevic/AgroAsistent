dedf3ad4bcdf24c61f4cdfaf330527a0c60c2961
Jovica Lesevic <jovica.lesevic@gmail.com>
2026-03-28 07:14:05 +0100

    fix: CORS zatvoren na specificne origine

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    process.env.CLIENT_URL
-  ].filter(Boolean)
+    "https://agroasistent-client.onrender.com"
+  ]
 }));
 
 app.use(express.json());
